48323ee5a02330156085f346e45e99116a16eda4
misha-solntcev <misha.solntcev@gmail.com>
2022-07-05 11:49:01 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Вариант 1
+В одномерном массиве, состоящем из n вещественных элементов, вычислить:
+ сумму элементов массива с нечетными номерами;
+ сумму элементов массива, расположенных между первым и последним
+отрицательными элементами.*/
+
+namespace _3_1_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
